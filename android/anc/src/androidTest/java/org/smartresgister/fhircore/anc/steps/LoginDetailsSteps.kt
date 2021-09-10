1b0c97a2faccc6b29369e83662f615664fd84253
naveen zehra <naveen.zehra@simplifipay.com>
2021-09-10 16:51:37 +0500

    Automation

--- android/anc/src/androidTest/java/org/smartresgister/fhircore/anc/steps/LoginDetailsSteps.kt
+++ android/anc/src/androidTest/java/org/smartresgister/fhircore/anc/steps/LoginDetailsSteps.kt
@@ -1,16 +1,31 @@
 package org.smartregister.fhircore.anc.steps
 
 
+import android.app.Application
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.ui.test.*
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.junit4.createEmptyComposeRule
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 
 import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider
 
 import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
+import androidx.ui.test.assertIsDisplayed
+import androidx.ui.test.findByText
 import org.smartregister.fhircore.anc.ui.login.LoginActivity
 import cucumber.api.java.After
 import cucumber.api.java.Before
@@ -18,53 +33,83 @@ import cucumber.api.java.en.And
 import cucumber.api.java.en.Given
 import cucumber.api.java.en.Then
 import cucumber.api.java.en.When
+import kotlinx.coroutines.launch
 import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
+import org.smartregister.fhircore.engine.R
+//import org.smartregister.fhircore.anc.R
 import org.smartregister.fhircore.anc.cucumber.espresso.login.LoginScreenRobot
-
+import org.smartregister.fhircore.engine.auth.AuthenticationService
+import org.smartregister.fhircore.engine.configuration.app.ConfigurableApplication
+import org.smartregister.fhircore.engine.configuration.view.loginViewConfigurationOf
+import org.smartregister.fhircore.engine.ui.login.BaseLoginActivity
+import org.smartregister.fhircore.engine.ui.login.LoginPage
+import org.smartregister.fhircore.engine.ui.login.LoginScreen
+import org.smartregister.fhircore.engine.ui.login.LoginViewModel
+import org.smartregister.fhircore.engine.ui.questionnaire.QuestionnaireActivity
+import org.smartregister.fhircore.engine.ui.questionnaire.QuestionnaireViewModel
+import org.smartregister.fhircore.engine.ui.theme.AppTheme
+import org.smartregister.fhircore.engine.util.extension.assertIsConfigurable
+import org.smartregister.fhircore.engine.util.extension.createFactory
+import org.smartregister.fhircore.engine.util.extension.runSync
 
 
 @RunWith(AndroidJUnit4::class)
-class LoginDetailsSteps{
+class LoginDetailsStep {
 
     private val robot = LoginScreenRobot()
-
-    @Rule
-    val composeTestRule = createComposeRule()
+    private lateinit var LoginViewModel: LoginViewModel
 
 
-//   @Rule
-//        val composeTestRule = createAndroidComposeRule(LoginActivity::class.java)
+    @get:Rule
+   val composeTestRule = createAndroidComposeRule<BaseLoginActivity>()
 
-//    @Rule
-//    var activityScenarioRule = ActivityScenarioRule(LoginActivity::class.java)
+
+//    @get:Rule
+//    val composeTestRule = createComposeRule()
+
+//    @get:Rule
+//    var activityScenarioRule: ActivityScenarioRule<LoginActivity> = ActivityScenarioRule(LoginActivity::class.java)
+//
 
     @Before
     fun setup() {
         val context = InstrumentationRegistry.getInstrumentation().targetContext
-        composeTestRule.setContent{ApplicationProvider.getApplicationContext<LoginActivity>() }
-     //   composeTestRule.setContent { LoginScreenPreview() }
+//        composeTestRule.setContent {
+//            AppTheme {
+//               LoginScreen(loginViewModel = LoginViewModel(ApplicationProvider.getApplicationContext(),(ApplicationProvider.getApplicationContext<Application>() as ConfigurableApplication).authenticationService, loginViewConfigurationOf()))
+//            }
+//        }
 
     }
 
     @After
     fun tearDown() {
         Intents.release() // Required for test scenarios with multiple activities or scenarios with more cases
+       // composeTestRule.activity.finish()
     }
+
 
     @Given("^I start the application$")
     fun i_start_app() {
         Intents.init()
         ActivityScenario.launch(LoginActivity::class.java)
+        Thread.sleep(5000)
+
+        }
 
 
-    }
 
     @When("^I click email field$")
+    @Test
     fun i_click_email_field() {
-        println(composeTestRule.onRoot().printToString())
-//        Thread.sleep(5000)
-//        composeTestRule.onNodeWithText("").performTextInput("demo")
+
+//        println("**************************************************************************************************")
+       composeTestRule.onNodeWithText("Enter username").assertExists()
+//        println("**************************************************************************************************))")
+//        InstrumentationRegistry.getInstrumentation().context
+//                .getString(R.string.username_input_hint)
 
     }
 
--- android/anc/src/androidTest/java/org/smartresgister/fhircore/anc/test/CucumberTestRunner.kt
+++ android/anc/src/androidTest/java/org/smartresgister/fhircore/anc/test/CucumberTestRunner.kt
@@ -3,8 +3,10 @@ package org.smartregister.fhircore.anc.test
 
 import android.os.Bundle
 import androidx.test.runner.AndroidJUnitRunner
+import com.fasterxml.jackson.core.JsonPointer.SEPARATOR
 import cucumber.api.CucumberOptions
 import cucumber.api.android.CucumberInstrumentationCore
+import java.io.File
 
 
 @CucumberOptions(features = ["features"],
@@ -16,13 +18,40 @@ class CucumberTestRunner : AndroidJUnitRunner() {
 
 private val instrumentationCore: CucumberInstrumentationCore? = CucumberInstrumentationCore(this)
 
+//    override fun onCreate(bundle: Bundle) {
+//        println("class invoked ")
+//        val optionsInBundle = setupCucumberOptionsInBundle(bundle)
+//        super.onCreate(optionsInBundle)
+//    }
+//
+//    private fun setupCucumberOptionsInBundle(bundle: Bundle): Bundle {
+//        val storageDirectory = getStorageDirectory()
+//
+//        bundle.putString("plugin", createRerunReportOptions(storageDirectory))
+//        return bundle
+//    }
+//
+//    private fun getStorageDirectory(): String {
+//        val directory = targetContext.getExternalFilesDir(null)
+//        return File( directory,"reports").absolutePath
+//    }
+//
+//    private fun createRerunReportOptions(storageDirectory: String): String {
+//        return "" +
+//                "junit:" + storageDirectory + "/cucumber-junit.xml" + SEPARATOR +
+//                "json:" + storageDirectory + "/cucumber-report-composite.json" + SEPARATOR +
+//                "html:" + storageDirectory + "/cucumber-report.html" + SEPARATOR +
+//                "rerun:" + storageDirectory + "/rerun.txt" + SEPARATOR +
+//                "pretty" + SEPARATOR
+//    }
+
     override fun onCreate(bundle: Bundle?) {
-        instrumentationCore!!.create(bundle)
+       instrumentationCore!!.create(bundle)
         super.onCreate(bundle)
     }
 
     override fun onStart() {
         waitForIdleSync()
-        instrumentationCore!!.start()
+       instrumentationCore!!.start()
     }
 }
